268c8913faa5620a29623b7396a7b7d840ac5662
Chris Barker <chris@Chriss-MBP.domain>
2017-03-13 16:45:45 -0700

    moved pip command to meta.yaml

--- wxpython-phoenix/bld.bat
+++
@@ -1,5 +0,0 @@
-pip install -U --pre \
-        --trusted-host wxpython.org \
-        -f http://wxpython.org/Phoenix/snapshot-builds/ \
-        wxPython_Phoenix
-
--- wxpython-phoenix/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pip install -U --pre \
-        --trusted-host wxpython.org \
-        -f http://wxpython.org/Phoenix/snapshot-builds/ \
-        wxPython_Phoenix==3.0.3.dev2832+e3fbb9f
-
